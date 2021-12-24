b06a9521dc2e548a29c2ae29136dcd3db70c825b
Marc-Antoine Parent <maparent@acm.org>
2021-12-24 08:29:34 -0500

    allow to use a postgres base image name other than postgres

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 cd /docker
-IMAGE_ID=$(docker build --quiet --build-arg INPUT_POSTGRES_IMAGE_TAG="$INPUT_POSTGRES_IMAGE_TAG" .)
+IMAGE_ID=$(docker build --quiet --build-arg INPUT_POSTGRES_IMAGE_TAG="$INPUT_POSTGRES_IMAGE_TAG" --build-arg INPUT_POSTGRES_IMAGE_NAME="$INPUT_POSTGRES_IMAGE_NAME" .)
 
 SERVICE_ID=$(docker run \
   -e POSTGRES_USER="${INPUT_POSTGRES_USER:-postgres}" \
